42807e6ca3462cba6f87b2332aa7a1110770892b
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-06-29 03:10:42 +0000

    looking good

--- src/components/EntityList.jsx
+++ src/components/EntityList.jsx
@@ -4,19 +4,36 @@ import useGlobalReducer from '/src/hooks/useGlobalReducer.jsx';
 import { addToFavorites } from '../actions';
 
 const EntityCard = ({ entity, addToFavorites }) => (
-  <div className="card" style={{ width: "200px", height: "250px", marginBottom: "10px", display: "flex", border: "3px black solid", color: "#ffe81f", backgroundColor: "#000000"  }}>
-    <img className="card-img-top" src={`https://starwars-visualguide.com/assets/img/characters/${entity.uid}.jpg`} alt={entity.name} width="100" height="100"/>   
-    <div className="card-body">
-      <div style={{ display: "flex" }}>
+  <div className="card" style={{ width: "300px", height: "410px", marginBottom: "10px", display: "flex", border: "3px black solid", color: "#ffe81f", backgroundColor: "#000000"  }}>
+    <img className="card-img-top" src={`https://starwars-visualguide.com/assets/img/characters/${entity.uid}.jpg`} alt={entity.name} width="100" height="225"/>   
+    <div className="card-body" style={{ height: "5px", paddingBottom: "0", marginBottom: "0" }}>
+      <div style={{ display: "flex", marginBottom: "0" }}>
         <div style={{ width: "85%" }}>
-          <h6 className="card-title">{entity.name}</h6>
+          <h6 className="card-title" style={{ marginBottom: "0"}}>{entity.name}</h6>
         </div>
         <button style={{ border: "none", marginLeft: "5px", marginRight: "0", backgroundColor: "#000000" }} onClick={() => addToFavorites(entity)}>❤️</button>
       </div>
     </div>    
-    
-    <Link to={`/details/${entity.uid}`} className="btn" style={{ fontSize: "14px", marginTop: "5px", marginRight: "0", marginLeft: "55px", marginBottom: "0", backgroundColor: "#ffe81f" }}>Learn more!</Link>
+    <p className="card-text" style={{ margin: "5px", fontSize: "12px" }}>Gender: {entity.gender}</p>
+    <p className="card-text" style={{ margin: "5px", fontSize: "12px" }}>Hair Color: {entity.hair_color}</p>
+    <p className="card-text" style={{ margin: "5px", fontSize: "12px" }}>Eye Color: {entity.eye_color}</p>
+    <Link to={`/details/${entity.uid}`} className="btn" style={{ width: "200px", fontSize: "14px", marginTop: "10px", marginRight: "auto", marginLeft: "auto", marginBottom: "10px", backgroundColor: "#ffe81f" }}>Learn more!</Link>
   </div>
+  // <div className="card" style={{ width: "300px", height: "410px", marginBottom: "10px", display: "flex", border: "3px black solid", color: "#ffe81f", backgroundColor: "#000000"  }}>
+  //   <img className="card-img-top" src={`https://starwars-visualguide.com/assets/img/vehicles/${entity.uid}.jpg`} alt={entity.name} width="100" height="225"/>   
+  //   <div className="card-body" style={{ height: "5px", paddingBottom: "0", marginBottom: "0" }}>
+  //     <div style={{ display: "flex", marginBottom: "0" }}>
+  //       <div style={{ width: "85%" }}>
+  //         <h6 className="card-title" style={{ marginBottom: "0"}}>{entity.name}</h6>
+  //       </div>
+  //       <button style={{ border: "none", marginLeft: "5px", marginRight: "0", backgroundColor: "#000000" }} onClick={() => addToFavorites(entity)}>❤️</button>
+  //     </div>
+  //   </div>    
+  //   <p className="card-text" style={{ margin: "5px", fontSize: "12px" }}>Model: {entity.model}</p>
+  //   <p className="card-text" style={{ margin: "5px", fontSize: "12px" }}>Crew: {entity.crew}</p>
+  //   <p className="card-text" style={{ margin: "5px", fontSize: "12px" }}>Passengers: {entity.passengers}</p>
+  //   <Link to={`/details/${entity.uid}`} className="btn" style={{ width: "200px", fontSize: "14px", marginTop: "10px", marginRight: "auto", marginLeft: "auto", marginBottom: "10px", backgroundColor: "#ffe81f" }}>Learn more!</Link>
+  // </div>
 );
 
 const EntityList = ({ entities }) => {
@@ -33,6 +50,6 @@ const EntityList = ({ entities }) => {
 
 export default EntityList;
 
-{/* <p className="card-text" style={{ margin: "0", fontSize: "12px" }}>Model: {vehicle.model}</p>
+{/* 
     <p className="card-text" style={{ margin: "0", fontSize: "12px" }}>Crew: {vehicle.crew}</p>
     <p className="card-text" style={{ margin: "0", fontSize: "12px" }}>Passangers: {vehicle.passangers}</p>  */}
