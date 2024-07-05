f829dec99d772b2da7b0ff8f561cab8c5cc89b57
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-07-05 13:41:17 +0000

    finished

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-dark" style={{backgroundColor: "#ffe81f", color: "#000"  }}>
-      <Link className="navbar-brand" to="/" style={{ fontFamily:  "Star Wars, sans-serif", marginLeft: "375px", fontSize: "30px", color: "#ffe81f" }}>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: "#ffe81f"  }}>
+      <Link className="navbar-brand" to="/" style={{ color: "#000000", fontFamily:  "death star, san-serif", marginLeft: "375px", fontSize: "40px" }}>
         Star Wars Databank
       </Link>
       <div className="collapse navbar-collapse">
         <div className="navbar-nav ml-auto">
-          <Link className="nav-link" to="/favorites" style={{ fontFamily:  "Star Wars, sans-serif", marginLeft: "200px", color: "#ffe81f" }}>
+          <Link className="nav-link" to="/favorites" style={{ fontSize: "20px", fontFamily:  "death star, san-serif", marginLeft: "200px", color: "#000000" }}>
             Favorites
           </Link>
         </div>
